4dac68db265b517de9cc0983b03e7a66fb117307
Loreto Gabawa Jr. <jun.rej@gmail.com>
2024-05-11 14:15:20 +0800

    update

--- API/v1/index.php
+++ API/v1/index.php
@@ -1,13 +1,21 @@
 <?php
 
   $request = $_REQUEST[ 'request' ];
-
+  $msg = "";
+  $response = '';
   if( isset( $request ) && !empty( $request )) {
     if( file_exists( "./requests/$request.php" ) ) {
       require_once "./requests/$request.php";
     } else {
-      echo 'Unknown request.';
+      $msg = 'Unknown request.';
     }
   } else {
-    echo 'Invalid Request.';
+    $msg = 'Invalid Request.';
   }
+
+
+header('Content-Type: application/json');
+echo json_encode( [
+  'message' => $msg,
+  'response' => $response
+] );
---
+++ API/v1/requests/alert.php
@@ -0,0 +1,7 @@
+<?php
+
+$user_id = $_GET[ 'user_id' ] ;
+$location = $_GET[ 'location'];
+
+
+/* select profile from user with user_id =123 */
