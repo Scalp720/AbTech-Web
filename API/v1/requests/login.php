3b087c7a16b3f5983c1eba9033c152a80fc0476e
Loreto Gabawa Jr. <jun.rej@gmail.com>
2024-05-17 12:13:56 +0800

    fixed login data received

--- API/v1/requests/login.php
+++ API/v1/requests/login.php
@@ -4,13 +4,27 @@ require_once '../../db.php';
 // Check if the request method is POST
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     // Retrieve the username and password from the request body
-    $username = isset( $_POST['username'] ) ? $_POST['username'] : null;
-    $password = isset( $_POST['password'] ) ? md5($_POST['password']) : null;
 
-    if( !$username || !$password ) {
+    $json_data = file_get_contents('php://input');
+    $request_data = json_decode($json_data);
+
+    /* $username = isset( $_POST['username'] ) ? $_POST['username'] : null;
+    $password = isset( $_POST['password'] ) ? md5($_POST['password']) : null; */
+
+    /* if( !$username || !$password ) {
       echo json_encode( [ 'message' => 'Username or password not found.', 'error' => 1 ] );
       exit;
+    } */
+
+    if ($request_data === null) {
+      // Failed to decode JSON
+      http_response_code(400); // Bad Request
+      echo json_encode(['error' => 'Invalid JSON data']);
+      exit();
     }
+
+    $username = $request_data->username;
+    $password = $request_data->password;
 
     // Prepare and execute the SQL query to check if the user exists
     $query = "SELECT * FROM " . PREF_ . "user WHERE username='$username' AND password='$password'";
